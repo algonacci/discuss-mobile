0bfc69bc6e054ded3e51987ed0ecbac1372ab180
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2022-12-23 21:15:15 +0700

    setup endpoint

---
+++ lib/config/api.dart
@@ -0,0 +1,10 @@
+class Api {
+  static const _baseUrl = 'http://192.168.1.111/discuss-backend';
+  static const comment = '$_baseUrl/comment';
+  static const follow = '$_baseUrl/follow';
+  static const topic = '$_baseUrl/topic';
+  static const user = '$_baseUrl/user';
+  static const imageComment = '$_baseUrl/image/comment';
+  static const imageTopic = '$_baseUrl/image/topic';
+  static const imageUser = '$_baseUrl/image/user';
+}
